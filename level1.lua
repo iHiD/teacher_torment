8985c4cbecfef1be1eef390f7136f43a82fdb3c2
Jeremy Walker <jez.walker@gmail.com>
2013-12-08 12:36:16 +0000

    Allow for students to pick things up and throw them

--- level1.lua
+++ level1.lua
@@ -8,11 +8,12 @@ local storyboard = require( "storyboard" )
 local scene = storyboard.newScene()
 local user
 local throwables = {}
+local selectedThrowable
 --local myText
 
-local beginX 
-local beginY  
-local endX  
+local beginX
+local beginY
+local endX
 local endY
 
 local moveUser = {}
@@ -44,13 +45,18 @@ local screenW, screenH, halfW = display.contentWidth, display.contentHeight, dis
 --
 -----------------------------------------------------------------------------------------
 
-local function gameListeners(action)
-  if(action == 'add') then
-    Runtime:addEventListener('accelerometer', moveUser)
-    Runtime:addEventListener("touch", swipe)
-  else
-  end
-end
+--------------------
+--------------------
+-- Game Functions --
+--------------------
+--------------------
+
+
+------------------------
+------------------------
+-- Movement Functions --
+------------------------
+------------------------
 
 local function stopUser()
   user.bodyType = "static"
@@ -71,20 +77,73 @@ function swipe(event)
 end
 
 function moveUser:accelerometer(e)
-  stopUser()
+  user.bodyType = "static"
+  user.bodyType = "dynamic"
   user.isAwake = true
+  user.rotation = 0
   user:applyForce( e.xGravity * 100, -e.yGravity * 100, user.x, user.y )
+
+  if selectedThrowable then
+    timer.performWithDelay(100, moveSelectableToUser)
+  end
 end
 
 function throw()
-  local throwable = throwables[1]
+  if not selectedThrowable then
+    return
+  end
+
+  local throw = selectedThrowable
+  selectedThrowable = nil
+
   local x = (endX - beginX) / 10
   local y = (endY - beginY) / 10
-  throwable:applyForce( x, y, throwable.x, throwable.y )
+  throw:applyForce( x, y, throw.x, throw.y )
 end
 
-local function addDesk( x, y )
+function moveSelectableToUser()
+  selectedThrowable.isAwake = true
+  selectedThrowable.rotation = 0
+  selectedThrowable.x = user.x + 20
+  selectedThrowable.y = user.y -20
+end
+
+local function onCollision( event )
+  if ( event.phase == "began" ) then
+    local collision_user
+    local collision_desk
+    if (event.object1.name == "User") then collision_user = event.object1 end
+    if (event.object2.name == "User") then collision_user = event.object2 end
+    if (event.object1.name == "Desk") then collision_desk = event.object1 end
+    if (event.object2.name == "Desk") then collision_desk = event.object2 end
+
+    if collision_user and collision_desk and (not selectedThrowable) and collision_desk.throwable then
+      selectedThrowable = collision_desk.throwable
+      collision_desk.throwable = nil
+      selectedThrowable:toFront()
+      timer.performWithDelay(100, moveSelectableToUser)
+    end
+  end
+end
+
+---------------------
+---------------------
+-- Setup Functions --
+---------------------
+---------------------
+
+local function gameListeners(action)
+  if(action == 'add') then
+    Runtime:addEventListener('accelerometer', moveUser)
+    Runtime:addEventListener("touch", swipe)
+    Runtime:addEventListener( "collision", onCollision )
+  else
+  end
+end
+
+local function addDesk( x, y, throwable_type )
 	local desk = display.newImageRect( "desk.png", 120, 90 )
+  desk.name = "Desk"
 	desk.x, desk.y = x, y
 
   left = -60
@@ -92,24 +151,55 @@ local function addDesk( x, y )
   top = -45
   bottom = 40
 
-  local shape = {left,top, right,top, right,bottom, left,bottom}
-  local material = {density=2, friction=1, bounce=0.1, shape=shape}
+  local material = {density=2, friction=1, bounce=0.1}
   physics.addBody(desk, 'static', material)
 
-  local paper = createPaper()
-	paper.x = x + 30
-  paper.y = y + 10
+  local throwable
+  if throwable_type == "paper" then
+    throwable = createPaper()
+  elseif throwable_type == "rubber" then
+    throwable = createRubber()
+  elseif throwable_type == "plane" then
+    throwable = createPlane()
+  elseif throwable_type == "can" then
+    throwable = createCan()
+  end
+  throwable.x = x + 30
+  throwable.y = y + 10
+  desk.throwable = throwable
+end
+
+function createCan()
+	local can = display.newImageRect( "can.png", 24, 40)
+  can.name = "Can"
+  setupThrowable(can)
+  return can
+end
+
+function createRubber()
+	local rubber = display.newImageRect( "rubber.png", 23, 16)
+  rubber.name = "Rubber"
+  setupThrowable(rubber)
+  return rubber
+end
+
+function createPlane()
+	local plane = display.newImageRect( "plane.png", 32, 26)
+  plane.name = "Plane"
+  setupThrowable(plane)
+  return plane
 end
 
 function createPaper()
 	local paper = display.newImageRect( "paper.png", 20, 20)
-  setupThrowable(paper, {radius=6})
+  paper.name = "Paper"
+  setupThrowable(paper)
   return paper
 end
 
-function setupThrowable(throwable, options)
+function setupThrowable(throwable)
   material = {density=0.1, friction=1, bounce=0.1}
-  for k,v in pairs(options) do material[k] = v end
+  --for k,v in pairs(options) do material[k] = v end
 
   physics.addBody( throwable, 'dynamic', material)
   throwable.isSensor = true
@@ -118,6 +208,7 @@ end
 
 local function addUser( view, x, y )
 	user = display.newImageRect( "kid.png", 50, 50)
+  user.name = "User"
 	user.x = screenW / 2
   user.y = screenH - 20
   physics.addBody( user, 'dynamic', {density=0.1, friction=1, bounce=0.1, radius=18})
@@ -125,12 +216,13 @@ end
 
 local function addTeacher( view, x, y )
 	teacher = display.newImageRect( "teacher.png", 70, 70)
+  teacher.name = "Teacher"
 	teacher.x = screenW / 2
   teacher.y = 70
   physics.addBody( teacher, 'dynamic', {density=0.1, friction=1, bounce=0.1, radius=36})
 end
 
-local function addWalls()
+local function addRoom()
 	local carpet = display.newImageRect( "carpet.png", screenW * 2, screenH * 2)
 	carpet.x = 0
   carpet.y = 0
@@ -152,14 +244,16 @@ end
 -- Called when the scene's view does not exist:
 function scene:createScene( event )
 
-  addWalls()
-  addDesk( 70, screenH -300)
-  addDesk( 230, screenH - 300)
-  addDesk( 160, screenH - 130)
+  addRoom()
+  addDesk( 70, screenH -300, 'plane')
+  addDesk( 230, screenH - 300, 'rubber')
+  addDesk( 160, screenH - 130, 'can')
   addUser()
   addTeacher()
 
   gameListeners('add')
+
+  user:applyForce( 5, -100, user.x, user.y )
 end
 
 
